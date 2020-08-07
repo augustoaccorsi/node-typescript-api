8f95af1a9ec8ea682fb20260f670d2ffa0c3e178
augustoaccorsi <augusto.acorsi@gmail.com>
2020-08-07 13:43:47 -0300

    add functional tests setup and server configuration

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const { resolve } = require('path');
+const root = resolve(__dirname);
+module.exports = {
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/src/**/*.test.ts'], // testes unitários ficarão dentro da pasta source
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1',
+  },
+};
