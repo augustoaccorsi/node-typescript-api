83516ff403fbcc884137ed26247ba3b424426e45
I851169 <augusto.accorsi@sap.com>
2022-03-12 18:19:36 -0300

    clean up project

--- src/util/errors/internal-erros.ts
+++
@@ -1,7 +0,0 @@
-export class InternalError extends Error {
-    constructor(public message: string, protected code: number = 50, protected description?: string){
-        super(message);
-        this.name = this.constructor.name;
-        Error.captureStackTrace(this, this.constructor);
-    }
-}
